c8f325975c066fc074977e56c2cc7d6a6f97e4b9
George chiong <sheenkeithkim@gmail.com>
2021-11-29 01:10:24 +0800

    user prof design

---
+++ lib/settings.dart
@@ -0,0 +1,16 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
